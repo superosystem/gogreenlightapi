24315ef9ddc45c7409831292826a94fdba4a4dde
Agus S. Mubarok <gusrylmubarok@gmail.com>
2022-09-15 16:29:33 +0700

    Make Skeleton Frontend

--- frontend-server/src/index.js
+++ frontend-server/src/index.js
@@ -1,17 +1,63 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class Card extends React.Component {
+  render() {
+      return (
+          <div className="col-md-6 col-lg-4 d-flex align-items-stretch">
+              <div className="card mb-3">
+                  <img className="card-img-top" src={this.props.img} alt={this.props.imgalt} />
+                  <div className="card-body">
+                      <h4 className="card-title">{this.props.productname}</h4>
+                      Price: <strong>{this.props.price}</strong>
+                      <p className="card-text">{this.props.desc}</p>
+                      <a href="#buy" className="btn btn-primary">Buy</a>
+                  </div>
+              </div>
+          </div>
+      );
+  }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class CardContainer extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          cards: []
+      };
+  }
+
+  componentDidMount() {
+      console.log('Component Did Mount Called: ' + new Date().toLocaleString());
+      fetch('cards.json')
+          .then(res => res.json())
+          .then((result) => {
+              console.log('Fetch...');
+              this.setState({
+                  cards: result
+              });
+          });
+  }
+
+  render() {
+      const cards = this.state.cards;
+      let items = cards.map(
+          card => <Card key={card.id} {...card} />
+      );
+      return (
+          <div className='container pt-4'>
+              <h3 className='text-center text-primary'>Products</h3>
+              <div className="pt-4 row">
+                  {items}
+              </div>
+          </div>
+      );
+  }
+}
+
+
+// =======================================
+const root = createRoot(document.getElementById('root'));
+
+root.render(<CardContainer />);
