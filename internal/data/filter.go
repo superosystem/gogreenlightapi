154975a5f3e8487e8f4f7f6bfe421b41afad24d9
Agus S. Mubarok <gusrylmubarok@gmail.com>
2022-12-29 10:18:25 +0700

    Sorting Lists

--- internal/data/filter.go
+++ internal/data/filter.go
@@ -1,12 +1,34 @@
 package data
 
-import "github.com/gusrylmubarok/gogreenlight-api/internal/validator"
+import (
+	"strings"
+
+	"github.com/gusrylmubarok/gogreenlight-api/internal/validator"
+)
 
 type Filters struct {
 	Page         int
 	PageSize     int
 	Sort         string
 	SortSafelist []string
+}
+
+func (f Filters) sortColumn() string {
+	for _, safeValue := range f.SortSafelist {
+		if f.Sort == safeValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+
+	panic("unsafe sort parameter: " + f.Sort)
+}
+
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+
+	return "ASC"
 }
 
 func ValidateFilters(v *validator.Validator, f Filters) {
