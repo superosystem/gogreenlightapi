0b5abdb96142ebd2d9d67d9686a106ad6ebfedff
Agus S. Mubarok <gusrylmubarok@gmail.com>
2023-01-02 09:18:19 +0700

    Graceful Shutdown

---
+++ README.md
@@ -0,0 +1,22 @@
+# GO Programming Language GreenLight API
+
+## Contents
+
+- [x] Sending JSON Responses
+- [x] Parsing JSON Requests
+- [x] Database Setup and Configuration
+- [x] SQL Migrations
+- [x] CRUD Operations
+- [x] Filtering, Sorting and Pagination
+- [x] Structured Logging and Error Handling
+- [x] Rate Limiting
+- [ ] Graceful Shutdown
+- [ ] User Mode Setup adn Registration
+- [ ] Sending Emails
+- [ ] User Activation
+- [ ] Authentication
+- [ ] Permission- []based Authorization
+- [ ] Cross Origin Requests
+- [ ] Metrics
+- [ ] Building, Versioning, and Quality Control
+- [ ] Deployment and Hosting
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,8 +4,6 @@ import (
 	"context"
 	"database/sql"
 	"flag"
-	"fmt"
-	"net/http"
 	"os"
 	"time"
 
@@ -25,6 +23,11 @@ type config struct {
 		maxIdleConns int
 		maxIdleTime  string
 	}
+	limiter struct {
+		rps     float64
+		burst   int
+		enabled bool
+	}
 }
 
 type application struct {
@@ -38,10 +41,16 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+
 	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://postgres:my-secret-pw@localhost/greenlight?sslmode=disable", "PostgreSQL DSN")
 	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
 	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
 	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
+
+	flag.Float64Var(&cfg.limiter.rps, "limiter-rps", 2, "Rate limiter maximum requests per second")
+	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
+	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
+
 	flag.Parse()
 
 	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
@@ -77,23 +86,11 @@ func main() {
 		models: data.NewModels(db),
 	}
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
-
-	srv := &http.Server{
-		Addr:         fmt.Sprintf(":%d", cfg.port),
-		Handler:      app.routes(),
-		IdleTimeout:  time.Minute,
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 30 * time.Second,
+	// Call app.serve() to start the server.
+	err = app.serve()
+	if err != nil {
+		logger.PrintFatal(err, nil)
 	}
-
-	logger.PrintInfo("starting server", map[string]string{
-		"addr": srv.Addr,
-		"env": cfg.env,
-	})
-	err = srv.ListenAndServe()
-	logger.PrintFatal(err, nil)
 }
 
 func openDB(cfg config) (*sql.DB, error) {
